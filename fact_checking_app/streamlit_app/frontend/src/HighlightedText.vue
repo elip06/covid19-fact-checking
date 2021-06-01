bec1e2b90f303b90da0330c22b557af49db2f984
Ellie <elitsa.pankovska@gmail.com>
2021-06-01 19:08:47 +0300

    Include different color mapping for the suspicious sentences based on the results from the Google Fact Check Tools

--- fact_checking_app/streamlit_app/frontend/src/HighlightedText.vue
+++ fact_checking_app/streamlit_app/frontend/src/HighlightedText.vue
@@ -1,13 +1,26 @@
 <template>
   <v-main class="wrapper" fill-height>
     <v-card id="highlighted-text-container" flat class="ml-2 mt-1" max-width="694" fill-height>
+      <span class="pb-0">The sentences, containing potentially dubious claims, are highlighted accordingly:</span>
+      <v-timeline
+        class="ml-n15 pt-1"
+        dense>
+    <v-timeline-item class="pb-1" small color="red lighten-2">False</v-timeline-item>
+    <v-timeline-item class="pb-1" small color="deep-orange lighten-2">
+      Mostly False
+    </v-timeline-item>
+    <v-timeline-item class="pb-1" small color="yellow lighten-2">Mixture</v-timeline-item>
+    <v-timeline-item class="pb-1" small color="lime lighten-2">MostlyTrue</v-timeline-item>
+    <v-timeline-item class="pb-1" small color="green lighten-2">True</v-timeline-item>
+    <v-timeline-item class="pb-3" small color="cyan lighten-2">Missing info/Unclear</v-timeline-item>
+  </v-timeline>
       <span v-for="(s, i) in args.sentences" :key="i">
        <span v-if="args.labels[i]==0">{{s}}</span>
        <v-tooltip v-else bottom>
         <template v-slot:activator="{ on, attrs }">
          <span
           style="border-radius: 3px;"
-          :class="args.labels[i]==1 ? 'orange lighten-4 m-1' : 'red lighten-4 m-1'"
+          :class="calculateColor(args.labels[i])"
           v-bind="attrs"
           v-on="on"
          >{{s}}</span>
@@ -38,6 +51,18 @@ export default {
   props: ["args"], // Arguments that are passed to the plugin in Python are accessible in props `args`. Here, we access the "name" arg.
   data() {
     return {
+      trueValues: ['true', 'accurate', 'unbiased', 'correct'],
+      mostlyTrueValues: ['mostly true', 'one pinocchio'],
+      mixtureValues: ['mixture', 'two pinocchios', 'biased', 'cherry-picking'],
+      mostlyFalseValues: ['mostly false', 'three pinocchios', 'misleading'],
+      falseValues: ['false', 'four pinocchios', 'inaccurate', 'miscaptioned', 'misattributed', 'scam'],
+      colorMapping: {
+        1: 'red lighten-2',
+        2: 'deep-orange lighten-2',
+        3: 'yellow lighten-2',
+        4: 'lime lighten-2',
+        5: 'green lighten-2'
+      }
     };
   },
   methods: {
@@ -49,6 +74,38 @@ export default {
       } else if (i == this.args.labels[sI].slice(0,5).length -1) {
         return 'mb-1'
       } else return ''
+    },
+    calculateColor: function(reviews) {
+      if (reviews == 1) {
+        return 'cyan lighten-4'
+      } else {
+        const ratings = []
+      reviews.forEach((review) => {
+        if (review.claimReview.length > 0) {
+          const rating = review.claimReview[0].textualRating;
+          const numericalRating = this.determineRating(rating.toLowerCase());
+          if (numericalRating) {
+            ratings.push(numericalRating);
+          }
+        }
+      });
+      const average = Math.round(eval(ratings.join('+'))/ratings.length);
+      return average ? this.colorMapping[average] : 'cyan lighten-4';
+      }
+
+    },
+    determineRating: function(textualRating) {
+      if (this.mostlyTrueValues.find(v => textualRating.includes(v))) {
+        return 4;
+      } else if (this.mostlyFalseValues.find(v => textualRating.includes(v))) {
+        return 2;
+      } else if (this.mixtureValues.find(v => textualRating.includes(v))) {
+        return 3;
+      } else if (this.falseValues.find(v => textualRating.includes(v))) {
+        return 1;
+      } else if (this.trueValues.find(v => textualRating.includes(v))) {
+        return 5;
+      } else return null;
     }
   },
   updated() {
@@ -71,6 +128,16 @@ export default {
   display: -webkit-box;
   -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
+}
+.v-timeline::before {
+  display: none;
+}
+.v-timeline-item__divider {
+  min-width: 40px !important;
+}
+.v-timeline-item__body {
+  margin-top: 3px !important;
+  font-size: 14px !important;
 }
 </style>
 
